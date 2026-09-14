36a1b78d6df84e62d2d18c92d11f72146af401a9
pyetrocastro-web <pyetro_castro@estudante.sesisenai.org.br>
2026-09-14 15:38:47 -0300

    em progresso

--- src/db.js
+++ src/db.js
@@ -1,13 +1,16 @@
+import pg from "pg";
+import dotenv from "dotenv";
 
-const { Pool } = require('pg');
-require('dotenv').config();
+dotenv.config();
+
+const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
-module.exports = pool;
+export default pool;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
-const pool = require('./db');
+import express from "express";
+import { listarEquipamentos } from "./service.js";
 
-async function testarConexao() {
+const app = express();
+
+app.use(express.json());
+
+app.get("/equipamentos", async (req, res) => {
     try {
-        const resultado = await pool.query(
-            'SELECT * FROM equipamentos ORDER BY id'
-        );
+        const equipamentos = await listarEquipamentos();
 
-        console.log('Conexão realizada com sucesso!');
-        console.table(resultado.rows);
-    } catch (erro) {
-        console.error('Erro ao consultar o banco:', erro.message);
-    } finally {
-        await pool.end();
+        res.json(equipamentos);
+    } catch (error) {
+        console.error("Erro ao consultar o banco:", error);
+
+        res.status(500).json({
+            erro: "Erro ao consultar o banco de dados"
+        });
     }
-}
+});
 
-testarConexao();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
